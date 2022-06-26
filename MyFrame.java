ec1c2c33603fead32ba2015d65889a0419494f85
Michele Pagano <mpagano284@gmail.com>
2022-06-26 22:36:13 +0200

    Risolto #1

--- MyFrame.java
+++ MyFrame.java
@@ -1,11 +1,12 @@
 import javax.swing.*;
 import java.awt.event.*;
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.awt.*;
-
 
 //TO DO: Aggiungere la scrollbar
 public class MyFrame extends JFrame implements ActionListener {
@@ -20,8 +21,8 @@ public class MyFrame extends JFrame implements ActionListener {
     JMenuItem exitItem = new JMenuItem("Exit");
     ImageIcon icon;
     JTextField textField = new JTextField();
-    JTextArea textArea = new JTextArea(300,1);
-    
+    JTextArea textArea = new JTextArea(300, 1);
+
     MyFrame() {
         this.setTitle("Txt Editor");
         this.setSize(800, 600);
@@ -38,27 +39,25 @@ public class MyFrame extends JFrame implements ActionListener {
         this.setResizable(false);
         this.add(textArea);
         this.add(textField);
-        
+
         this.setJMenuBar(menuBar);
         this.setVisible(true);
     }
 
-
     /*
      * TEXTFIELD
      */
-    private void setTextField(){
-        textField.setPreferredSize(new Dimension(800,200));
+    private void setTextField() {
+        textField.setPreferredSize(new Dimension(800, 200));
         textField.setForeground(Color.black);
         textField.setBackground(Color.white);
         textField.setEditable(true);
     }
 
-
     /*
      * FILE MENU
      */
-    public void setFileMenu(){
+    public void setFileMenu() {
         openItem.addActionListener(this);
         saveItem.addActionListener(this);
         saveAsItem.addActionListener(this);
@@ -79,72 +78,78 @@ public class MyFrame extends JFrame implements ActionListener {
         fileMenu.add(exitItem);
     }
 
-
     /*
      * TEXTAREA
      */
-    public void setTextArea(){
-        textArea.setPreferredSize(new Dimension(780,200));
+    public void setTextArea() {
+        textArea.setPreferredSize(new Dimension(780, 200));
         textArea.setForeground(Color.black);
         textArea.setBackground(Color.white);
-        textArea.setEditable(true); 
+        textArea.setEditable(true);
 
         textArea.setLineWrap(true);
         textArea.setWrapStyleWord(true);
 
     }
 
-
     /*
      * ICON
      */
-    public void setIcon(){
+    public void setIcon() {
         icon = new ImageIcon("icon.png");
         this.setIconImage(icon.getImage());
     }
 
-
     /*
      * MENUBAR
      */
-    public void setMenuBar(){
+    public void setMenuBar() {
         menuBar.add(fileMenu);
         menuBar.add(editMenu);
     }
 
-
     @Override
     public void actionPerformed(ActionEvent e) {
         JFileChooser fileChooser;
-        
+
         /*
          * NEW WINDOW
          */
-        if(e.getSource() == newWindowItem) {
+        if (e.getSource() == newWindowItem) {
 
-            //TO DO: Fare in modo che se si chiude una finestra non si chiudano tutte
+            // TO DO: Fare in modo che se si chiude una finestra non si chiudano tutte
 
             MyFrame newFrame = new MyFrame();
             newFrame.setVisible(true);
 
-            System.out.println("New Window");            
+            System.out.println("New Window");
         }
 
         /*
          * OPEN
          */
-        if(e.getSource() == openItem) {
+        if (e.getSource() == openItem) {
 
-            //TO DO: Visualizzare il contenuto del file txt
+            // TO DO: Visualizzare il contenuto del file txt
 
             fileChooser = new JFileChooser();
-            fileChooser.setCurrentDirectory(new File("C://Users//Utente//Documents"));
+            fileChooser.setCurrentDirectory(new File(System.getProperty("user.dir")));
 
             int response = fileChooser.showOpenDialog(this);
 
             if (response == JFileChooser.APPROVE_OPTION) {
                 File file = new File(fileChooser.getSelectedFile().getAbsolutePath());
-                System.out.println(file);
+                try (FileReader fr = new FileReader(file);
+                        BufferedReader br = new BufferedReader(fr)) {
+
+                    String valueRead = br.readLine();
+                    while (valueRead != null) {
+                        textArea.append(valueRead + "\n");
+                        valueRead = br.readLine();
+                    }
+                } catch (IOException ie) {
+                    ie.printStackTrace();
+                }
             }
 
             System.out.println("Open Item");
@@ -153,26 +158,23 @@ public class MyFrame extends JFrame implements ActionListener {
         /*
          * SAVE
          */
-        if(e.getSource() == saveItem) {
+        if (e.getSource() == saveItem) {
             fileChooser = new JFileChooser();
-            fileChooser.setCurrentDirectory(new File("C://Users//Utente//Documents"));
+            fileChooser.setCurrentDirectory(new File(System.getProperty("user.dir")));
 
             int response = fileChooser.showSaveDialog(this);
 
             if (response == JFileChooser.APPROVE_OPTION) {
                 File file = new File(fileChooser.getSelectedFile().getAbsolutePath());
-                try(FileWriter fw = new FileWriter(file);
-                    BufferedWriter bw = new BufferedWriter(fw)){
-                    if(!file.exists())
+                try (FileWriter fw = new FileWriter(file);
+                        BufferedWriter bw = new BufferedWriter(fw)) {
+                    if (!file.exists())
                         file.createNewFile();
-                    
-                    bw.write(this.textArea.getText() + "\n");
-                    bw.close();
+
+                    bw.write(this.textArea.getText());
                 } catch (IOException ie) {
                     ie.printStackTrace();
                 }
-
-                System.out.println(file);
             }
             System.out.println("Save Item");
         }
@@ -180,18 +182,18 @@ public class MyFrame extends JFrame implements ActionListener {
         /*
          * SAVE AS
          */
-        if(e.getSource() == saveAsItem) {
-            //TO DO: Salvare il file txt con un nome diverso
+        if (e.getSource() == saveAsItem) {
+            // TO DO: Salvare il file txt con un nome diverso
             System.out.println("Save As Item");
         }
 
         /*
          * EXIT
          */
-        if(e.getSource() == exitItem) {
+        if (e.getSource() == exitItem) {
             System.exit(0);
         }
-        
+
     }
-    
+
 }
